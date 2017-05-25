c0594955a268d7c3e45b43b9dd4805643e5a52be
leeyunfan <leeyunfans@163.com>
2017-05-26 02:26:44 +0800

    aaa

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-;; Added by Package.el.  This must come before configurations of
+\;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
@@ -12,11 +12,18 @@
 ;; install && manage package
 (require 'cl)
 (defvar faaaar/packages '(
+			  exec-path-from-shell
+			  nodejs-repl
+			  js2-mode
+			  smartparens
 			  company
 			  monokai-theme
 			  hungry-delete
+			  swiper
+			  counsel
 			  ) "Default Packages")
 
+(setq package-selected-packages faaaar/packages)
 (defun faaaar/packages-installed-p ()
   (loop for pkg in faaaar/packages
 	when (not (package-installed-p pkg)) do (return nil)
@@ -46,6 +53,9 @@
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 (global-hl-line-mode t)
 (load-theme 'monokai t)
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-c") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
 
 ;; add some hook
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
@@ -59,6 +69,36 @@
 ;; hungry-delete
 (require 'hungry-delete)
 (global-hungry-delete-mode)
+
+;; swiper config
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "<f6>") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+;; smartparens config
+(require 'smartparens-config)
+(smartparens-global-mode t)
+
+;; exec-path-from-shell config
+(require 'exec-path-from-shell) ;; if not using the ELPA package
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; js2-mode config
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+
+;; nodejs-repl
+(require 'nodejs-repl)
 
 ;; org config
 (require 'org)
@@ -82,7 +122,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company))))
+ '(exec-path-from-shell-check-startup-files nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
