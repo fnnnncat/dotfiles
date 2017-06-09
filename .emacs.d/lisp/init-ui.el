3c1b271e6c9a63340650be92aae244df8f74b871
leeyunfan <liyunfan1@xiaomi.com>
2017-06-09 22:09:05 +0800

    update

--- .emacs.d/lisp/init-ui.el
+++ .emacs.d/lisp/init-ui.el
@@ -1,37 +1,22 @@
 ;; cursor's type
-;; I hate block cursor
+;; 修改光标类型
 (setq-default cursor-type 'bar)
-;; default font style
+;; 设置默认字体
 (set-face-attribute 'default nil
                     :family "Source Code Pro"
                     :height 180
                     :weight 'normal
                     :width 'normal)
 
-;; solarized
+;; 主题solarized
 (require 'solarized)
-;; make the fringe stand out from the background
 (setq solarized-distinct-fringe-background t)
-
-;; Don't change the font for some headings and titles
 (setq solarized-use-variable-pitch nil)
-
-;; make the modeline high contrast
 (setq solarized-high-contrast-mode-line t)
-
-;; Use less bolding
-(setq solarized-use-less-bold t)
-
-;; Use more italics
+(setq solarized-use-less-bold nil)
 (setq solarized-use-more-italic t)
-
-;; Use less colors for indicators such as git:gutter, flycheck and similar
 (setq solarized-emphasize-indicators nil)
-
-;; Don't change size of org-mode headlines (but keep other size-changes)
 (setq solarized-scale-org-headlines nil)
-
-;; Avoid all font-size changes
 (setq solarized-height-minus-1 1.0)
 (setq solarized-height-plus-1 1.0)
 (setq solarized-height-plus-2 1.0)
